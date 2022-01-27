8da8bc89bc1e32dab1f39eea16aa3285cc51c04d
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2022-01-28 00:08:13 +0600

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php 
  
 $bdd = new PDO(
-    	"mysql:host=" . getenv("b45prdsvk4ln4ywqprxt-mysql.services.clever-cloud.com:3306") . ";dbname=" . getenv("b45prdsvk4ln4ywqprxt"),
-    	getenv("ufvpdfl30ruupkkv"),
-    	getenv("wHAKwN8NwmpjzoyRvaVJ")
+    	"mysql:host='b45prdsvk4ln4ywqprxt-mysql.services.clever-cloud.com:3306';dbname='b45prdsvk4ln4ywqprxt',
+    	'ufvpdfl30ruupkkv',
+    	'wHAKwN8NwmpjzoyRvaVJ'
   );
 // $conn = new mysqli("localhost", "ufvpdfl30ruupkkv", "wHAKwN8NwmpjzoyRvaVJ", "b45prdsvk4ln4ywqprxt");
